6ad07feb15a02fee1d129d924ad2aa0e1ba98e40
abraoribeiro <tecnicoabrao@hotmail.com>
2017-04-10 08:23:15 -0300

    novo

--- src/main/java/com/loja/beans/FormaPgto.java
+++ src/main/java/com/loja/beans/FormaPgto.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class FormaPgto {
 
 	@Id
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private int numMaxParc;
 	private String descricao;
